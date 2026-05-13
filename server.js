43ebaa2a73dd61483d3da51e3be39a360225c667
Taxaccsoft <taxaccsoftindia@gmail.com>
2026-05-13 23:36:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -194,8 +194,8 @@ const transporter = nodemailer.createTransport({
   secure: true,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: queryadon@gmail.com,
+    pass: ikbr olvc kdlf tyau,
   },
 });
 
