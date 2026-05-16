acf1b29c72f13bdd392f5cd5e22cce96d0bbb23e
Taxaccsoft <taxaccsoftindia@gmail.com>
2026-05-16 18:49:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,10 +33,15 @@ const upload = multer({
 
 // ✅ Nodemailer transporter (Gmail App Password REQUIRED)
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // VERY IMPORTANT (false for 587)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
